895681388cc7452c34d1c2b5d3bfa605b73ff98b
Manhã <joaoggbs62@gmail.com>
2024-11-05 12:06:59 -0300

    adw

--- L4/main.py
+++ L4/main.py
@@ -1,7 +1,7 @@
 from controllers.Controllers import MercadoLivreDB
 
 def main():
-    TOKEN = "YOUR_ASTRA_TOKEN"
+    TOKEN = "AstraCS:fyIJCEQXkwEjObmeqeqZaInm:44c960d3bd9b3a87cd3e2b605739fff3ceeb14acb23af63f5fc9fbca031cff15"
     API_ENDPOINT = "https://9f3876e3-09f2-44c9-98ea-3eea980fdf24-us-east-2.apps.astra.datastax.com"
     
     db = MercadoLivreDB(TOKEN, API_ENDPOINT)
